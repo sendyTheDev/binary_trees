aba98a4b7b5c69da9a9e50dfbc2b7b912a5be28b
sendyTheDev <phathisanisendy@gmail.com>
2023-08-31 18:26:15 -0700

    advance

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t binary_tree_height(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 heap_t *heap_insert(heap_t **root, int value);
 #endif
